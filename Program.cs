e0e96b080595fa1bfcb55f359d56107648ce03d0
caphuutuan <caphuutuan1@gmail.com>
2025-04-06 00:56:15 +0700

    Queue and Stack

--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
-﻿class Program{
-    static void Main(string[] args)
+﻿class Program
+{
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+    //    Queue<string> hoso_canxuly = new Queue<string>();
+    //     hoso_canxuly.Enqueue("Ho so 1");
+    //     hoso_canxuly.Enqueue("Ho so 2");
+    //     hoso_canxuly.Enqueue("Ho so 3");
+
+    //     while (hoso_canxuly.Count > 0)
+    //     {
+    //         var hoso = hoso_canxuly.Dequeue();
+    //         Console.WriteLine($"Dang xu ly ho so {hoso}, con lai {hoso_canxuly.Count} ho so trong hang cho xu ly.");
+    //     }
+
+    Stack<string> hoso_canxuly = new Stack<string> ();
+
+      hoso_canxuly.Push ("Hồ sơ A"); 
+      hoso_canxuly.Push ("Hồ sơ B"); 
+      hoso_canxuly.Push ("Hồ sơ C");
+
+      while (hoso_canxuly.Count > 0) {
+        var hs = hoso_canxuly.Pop();
+        Console.WriteLine($"Xử lý {hs}, còn lại {hoso_canxuly.Count}");
+      }
     }
 }
+
